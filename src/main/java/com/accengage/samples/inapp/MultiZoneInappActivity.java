ef1552b68509e39dcb58ba88c2d9c74e09c6c07f
Andrei Mandychev <amandychev@accengage.com>
2016-10-18 16:11:16 +0200

    Don't use the deprecated annotation UseA4S

--- src/main/java/com/accengage/samples/inapp/MultiZoneInappActivity.java
+++ src/main/java/com/accengage/samples/inapp/MultiZoneInappActivity.java
@@ -3,10 +3,8 @@ package com.accengage.samples.inapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.a4s.sdk.plugins.annotations.UseA4S;
 import com.accengage.samples.R;
 
-@UseA4S
 public class MultiZoneInappActivity extends AppCompatActivity {
 
     @Override
@@ -14,4 +12,6 @@ public class MultiZoneInappActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi_zone_inapp);
     }
+
+    // TODO integrate the SDK
 }
